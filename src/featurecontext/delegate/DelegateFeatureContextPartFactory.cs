ad341dd987955128dccd06ec3acce381c75f141e
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2017-11-26 19:38:38 +0100

    Add logging to the delegate-based feature context part factory

--- src/featurecontext/delegate/DelegateFeatureContextPartFactory.cs
+++ src/featurecontext/delegate/DelegateFeatureContextPartFactory.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using MAF.FeaturesFlipping.Extensibility.Activators;
 using MAF.FeaturesFlipping.Extensibility.FeatureContext;
+using Microsoft.Extensions.Logging;
 
 namespace MAF.FeaturesFlipping.FeatureContext.Delegate
 {
@@ -14,17 +15,21 @@ namespace MAF.FeaturesFlipping.FeatureContext.Delegate
             Func<IFeatureContext, Task> addFeatureContextPart,
             Action<IFeatureContext> releaseFeatureContextPart)
         {
-            _addFeatureContextPart = addFeatureContextPart ??  throw new ArgumentNullException(nameof(addFeatureContextPart));
+            _addFeatureContextPart = addFeatureContextPart ?? throw new ArgumentNullException(nameof(addFeatureContextPart));
             _releaseFeatureContextPart = releaseFeatureContextPart ?? throw new ArgumentNullException(nameof(releaseFeatureContextPart));
         }
 
         public Task AddFeatureContextPartAsync(IFeatureContext featureContext)
         {
+            var logger = (ILogger<FeatureSpec>)featureContext.FeaturesServices.GetService(typeof(ILogger<FeatureSpec>));
+            logger.AddPartToFeatureContext();
             return _addFeatureContextPart(featureContext);
         }
 
         public void ReleaseFeatureContextPart(IFeatureContext featureContext)
         {
+            var logger = (ILogger<FeatureSpec>)featureContext.FeaturesServices.GetService(typeof(ILogger<FeatureSpec>));
+            logger.ReleasePartToFeatureContext();
             _releaseFeatureContextPart(featureContext);
         }
     }
---
+++ src/featurecontext/delegate/LoggingExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace MAF.FeaturesFlipping.FeatureContext.Delegate
+{
+    internal static class LoggingExtensions
+    {
+        private static readonly Action<ILogger, Exception> _addPartToFeatureContext;
+        private static readonly Action<ILogger, Exception> _releasePartToFeatureContext;
+
+        static LoggingExtensions()
+        {
+            int eventId = 1;
+            _addPartToFeatureContext = LoggerMessage.Define(
+                LogLevel.Debug,
+                new EventId(eventId++, nameof(AddPartToFeatureContext)),
+                "Add part to feature context via delegate.");
+            _releasePartToFeatureContext = LoggerMessage.Define(
+                LogLevel.Debug,
+                new EventId(eventId++, nameof(ReleasePartToFeatureContext)),
+                "Release part from feature context via delegate.");
+        }
+        public static void AddPartToFeatureContext(this ILogger logger)
+        {
+            _addPartToFeatureContext(logger, null);
+        }
+        public static void ReleasePartToFeatureContext(this ILogger logger)
+        {
+            _releasePartToFeatureContext(logger, null);
+        }
+    }
+}
