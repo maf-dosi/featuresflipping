224784ce09d4624eaba71b14e0b8011c78d9f87e
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2017-11-29 10:45:22 +0100

    Add logging to the global configuration activator

--- src/activators/configuration/GlobalConfigurationFeature.cs
+++ src/activators/configuration/GlobalConfigurationFeature.cs
@@ -2,6 +2,8 @@
 using System.Threading.Tasks;
 using MAF.FeaturesFlipping.Extensibility.Activators;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace MAF.FeaturesFlipping.Activators.Configuration
 {
@@ -11,26 +13,26 @@ namespace MAF.FeaturesFlipping.Activators.Configuration
 
         public GlobalConfigurationFeature(IConfigurationSection configurationSection)
         {
-            _configurationSection = configurationSection ??throw new ArgumentNullException(nameof(configurationSection));
+            _configurationSection = configurationSection ?? throw new ArgumentNullException(nameof(configurationSection));
         }
 
         public Task<FeatureActivationStatus> GetStatusAsync(IFeatureContext featureContext)
         {
+            var logger = featureContext.FeaturesServices.GetService<ILogger<FeatureSpec>>();
             var value = _configurationSection.Value;
-            if (string.IsNullOrEmpty(value))
+            logger.RetrieveValueOfConfigurationSection(value);
+            if (string.IsNullOrEmpty(value) || !bool.TryParse(value, out var isActive))
             {
+                logger.ReturnNotSetActivationStatus();
                 return Task.FromResult(FeatureActivationStatus.NotSet);
             }
-
-            if (bool.TryParse(value, out var isActive))
+            if (isActive)
             {
-                if (isActive)
-                {
-                    return Task.FromResult(FeatureActivationStatus.Active);
-                }
-                return Task.FromResult(FeatureActivationStatus.Inactive);
+                logger.ReturnActiveActivationStatus();
+                return Task.FromResult(FeatureActivationStatus.Active);
             }
-            return Task.FromResult(FeatureActivationStatus.NotSet);
+            logger.ReturnInactiveActivationStatus();
+            return Task.FromResult(FeatureActivationStatus.Inactive);
         }
     }
 }
